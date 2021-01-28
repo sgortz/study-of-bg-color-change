ecb2890dde521656c08f0d8b62709153734f93a9
sgortz <sgortz@users.noreply.github.com>
2021-01-27 19:40:27 -0600

    Form input updates h1

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
-let apiKey = "4cea025489823b86da62835c695c95d3";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+// Updating the temperature and icon from page
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+function search(cityName){
+    let apiKey = "4cea025489823b86da62835c695c95d3";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(displayCity);
+}
+
 
 function displayCity(response) {
-  console.log(response.data);
   let forecast = document.querySelector("#weather-description");
   forecast.innerHTML = response.data.weather[0].description;
 
@@ -10,8 +22,17 @@ function displayCity(response) {
   temperature.innerHTML = Math.round(response.data.main.temp);
 
   let weatherIcon = document.querySelector("#weather-icon");
-  weatherIcon.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  weatherIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  weatherIcon.setAttribute("alt", response.data.weather[0].description);
+
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
 }
 
-axios.get(apiUrl).then(displayCity);
+search("New York");
 
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
