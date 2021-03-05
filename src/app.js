48851d84d3f31eab7fb87ae7630b1476612027e5
sgortz <sgortz@users.noreply.github.com>
2021-03-05 12:58:45 -0600

    changed array to object

--- src/app.js
+++ src/app.js
@@ -42,24 +42,17 @@ let forecast = [
   "Squall",
 ];
 
-let colors = [
-  //clear sky daylight
-  "linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%)",
-  //clear sky nighttime -- font color whitesmoke
-  "linear-gradient(to top, #09203f 0%, #537895 100%)",
-  //rainy or cloudy
-  "linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)",
-  //sand, Dust
-  "linear-gradient(135deg, #fdfcfb 0%, #e2d1c3 100%);",
-  //thunderstorm
-  "linear-gradient(to left, #BDBBBE 0%, #9D9EA3 100%), radial-gradient(88% 271%, rgba(255, 255, 255, 0.25) 0%, rgba(254, 254, 254, 0.25) 1%, rgba(0, 0, 0, 0.25) 100%), radial-gradient(50% 100%, rgba(255, 255, 255, 0.30) 0%, rgba(0, 0, 0, 0.30) 100%)",
-  // Morning
-  "linear-gradient(to top, #a8edea 0%, #fed6e3 100%)",
-];
+let colors = {
+  day: "linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%)",
+  night: "linear-gradient(to top, #09203f 0%, #537895 100%)",
+  rainyOrCloudy: "linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)",
+  sandOrDust: "linear-gradient(135deg, #fdfcfb 0%, #e2d1c3 100%);",
+  thunderstorm:
+    "linear-gradient(to left, #BDBBBE 0%, #9D9EA3 100%), radial-gradient(88% 271%, rgba(255, 255, 255, 0.25) 0%, rgba(254, 254, 254, 0.25) 1%, rgba(0, 0, 0, 0.25) 100%), radial-gradient(50% 100%, rgba(255, 255, 255, 0.30) 0%, rgba(0, 0, 0, 0.30) 100%)",
+  mornings: "linear-gradient(to top, #a8edea 0%, #fed6e3 100%)",
+};
 
 function displayCity(response) {
-  console.log(response.data.weather[0].main);
-  console.log(forecast[2]);
   let forecastElement = document.querySelector("#weather-description");
   forecastElement.innerHTML = response.data.weather[0].description;
 
@@ -79,18 +72,18 @@ function displayCity(response) {
   let apiIconResponse = response.data.weather[0].icon;
   let colorBackground = document.querySelector("div");
   if (apiIconResponse.includes("n")) {
-    colorBackground.style.backgroundImage = colors[1];
+    colorBackground.style.backgroundImage = colors.night;
     colorBackground.style.color = "whitesmoke";
   } else if (response.data.weather[0].main === forecast[0] && hour < 12) {
-    colorBackground.style.backgroundImage = colors[5];
+    colorBackground.style.backgroundImage = colors.mornings;
   } else if (response.data.weather[0].main === forecast[0] && hour >= 12) {
-    colorBackground.style.backgroundImage = colors[0];
+    colorBackground.style.backgroundImage = colors.day;
   } else if (response.data.weather[0].main === forecast[(10, 11)]) {
-    colorBackground.style.backgroundImage = colors[3];
+    colorBackground.style.backgroundImage = colors.sandOrDust;
   } else if (response.data.weather[0].main === forecast[(12, 13)]) {
-    colorBackground.style.backgroundImage = colors[4];
+    colorBackground.style.backgroundImage = colors.thunderstorm;
   } else {
-    colorBackground.style.backgroundImage = colors[2];
+    colorBackground.style.backgroundImage = colors.rainyOrCloudy;
   }
 }
 
